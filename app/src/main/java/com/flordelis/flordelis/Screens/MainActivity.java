87a7297e6fbd3fa19335cc820fe3cacb03f39c42
edwardcosta <eduardocalandrini@hotmail.com>
2018-01-19 18:29:36 -0200

    Serarch

--- app/src/main/java/com/flordelis/flordelis/Screens/MainActivity.java
+++ app/src/main/java/com/flordelis/flordelis/Screens/MainActivity.java
@@ -1,13 +1,30 @@
 package com.flordelis.flordelis.Screens;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
@@ -19,6 +36,7 @@ import com.github.ksoichiro.android.observablescrollview.ObservableRecyclerView;
 import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
 import com.github.ksoichiro.android.observablescrollview.ScrollState;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,8 +46,12 @@ public class MainActivity extends AppCompatActivity {
 
     private FragmentManager fragmentManager;
     private Toolbar toolbar;
+    private Toolbar searchtollbar;
 
     private SimpleDraweeView _userImg;
+
+    private Menu search_menu;
+    private MenuItem item_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("");
+        setSearchtollbar();
 
         _userImg = (SimpleDraweeView) toolbar.findViewById(R.id.activity_main_user_img);
 
@@ -72,6 +95,179 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
             }
-        });*/
+       });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_status:
+                Toast.makeText(this, "Home Status Click", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_search:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                    circleReveal(R.id.searchtoolbar,1,true,true);
+                else
+                    searchtollbar.setVisibility(View.VISIBLE);
+
+                item_search.expandActionView();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this, "Home Settings Click", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void setSearchtollbar() {
+        searchtollbar = (Toolbar) findViewById(R.id.searchtoolbar);
+        if (searchtollbar != null) {
+            searchtollbar.inflateMenu(R.menu.menu_search);
+            search_menu=searchtollbar.getMenu();
+
+            searchtollbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                        circleReveal(R.id.searchtoolbar,1,true,false);
+                    else
+                        searchtollbar.setVisibility(View.GONE);
+                }
+            });
+
+            item_search = search_menu.findItem(R.id.action_filter_search);
+
+            MenuItemCompat.setOnActionExpandListener(item_search, new MenuItemCompat.OnActionExpandListener() {
+                @Override
+                public boolean onMenuItemActionCollapse(MenuItem item) {
+                    // Do something when collapsed
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        circleReveal(R.id.searchtoolbar,1,true,false);
+                    }
+                    else
+                        searchtollbar.setVisibility(View.GONE);
+                    return true;
+                }
+
+                @Override
+                public boolean onMenuItemActionExpand(MenuItem item) {
+                    // Do something when expanded
+                    return true;
+                }
+            });
+
+            initSearchView();
+
+
+        } else
+            Log.d("toolbar", "setSearchtollbar: NULL");
+    }
+
+    public void initSearchView() {
+        final SearchView searchView =
+                (SearchView) search_menu.findItem(R.id.action_filter_search).getActionView();
+
+        // Enable/Disable Submit button in the keyboard
+
+        searchView.setSubmitButtonEnabled(false);
+
+        // Change search close button image
+
+        ImageView closeButton = (ImageView) searchView.findViewById(R.id.search_close_btn);
+        closeButton.setImageResource(R.drawable.ic_close);
+
+
+        // set hint and the text colors
+
+        EditText txtSearch = ((EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text));
+        txtSearch.setHint("Pesquisar..");
+        txtSearch.setHintTextColor(Color.DKGRAY);
+        txtSearch.setTextColor(getResources().getColor(R.color.colorPrimary));
+
+
+        // set the cursor
+
+        AutoCompleteTextView searchTextView = (AutoCompleteTextView) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        try {
+            Field mCursorDrawableRes = TextView.class.getDeclaredField("mCursorDrawableRes");
+            mCursorDrawableRes.setAccessible(true);
+            mCursorDrawableRes.set(searchTextView, R.drawable.search_cursor); //This sets the cursor resource ID to 0 or @null which will make it visible on white background
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                callSearch(query);
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                callSearch(newText);
+                return true;
+            }
+
+            public void callSearch(String query) {
+                //Do searching
+                Log.i("query", "" + query);
+
+            }
+
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void circleReveal(int viewID, int posFromRight, boolean containsOverflow, final boolean isShow)
+    {
+        final View myView = findViewById(viewID);
+
+        int width=myView.getWidth();
+
+        if(posFromRight>0)
+            width-=(posFromRight*getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_material))-(getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_material)/ 2);
+        if(containsOverflow)
+            width-=getResources().getDimensionPixelSize(R.dimen.abc_action_button_min_width_overflow_material);
+
+        int cx=width;
+        int cy=myView.getHeight()/2;
+
+        Animator anim;
+        if(isShow)
+            anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0,(float)width);
+        else
+            anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, (float)width, 0);
+
+        anim.setDuration((long)220);
+
+        // make the view invisible when the animation is done
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if(!isShow)
+                {
+                    super.onAnimationEnd(animation);
+                    myView.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        // make the view visible and start the animation
+        if(isShow)
+            myView.setVisibility(View.VISIBLE);
+
+        // start the animation
+        anim.start();
     }
 }
